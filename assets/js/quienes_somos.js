9217d4cf35d2fe2f8e43445326383a5706bc3bc4
JavierJDemos <javierjdemos@gmail.com>
2024-04-22 23:41:28 -0300

    cambios mariano y otros conceptos

--- assets/js/quienes_somos.js
+++ assets/js/quienes_somos.js
@@ -15,7 +15,7 @@ function displayData(data) {
   for (var i in data.quienes_somos) {
     tag1 = data.quienes_somos[i].textspan;
     tag2 = data.quienes_somos[i].textp;
-    dato = "<div><h4>" + tag1 + "</h4><p>" + tag2 + "</p></div><div><br></div>";
+    dato = "<div><h3>" + tag1 + "</h3><p>" + tag2 + "</p></div><div><br></div>";
     text1 += dato
   }
   qs.innerHTML = text1
